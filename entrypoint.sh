d409d3736a5ca42aaf03726eaa32f8e01dac457a
Igor Rodionov <goruha@gmail.com>
2022-11-16 00:40:37 +0300

    Added wrong yaml test

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,5 @@ set -o pipefail
 echo "${CONFIG}" | \
 	yq  -o json -M -e | \
 	jq -c -e -M  "${QUERY} | to_entries | map(\"\(.key)=\(.value|tostring)\")|.[]" | \
-	sed -e 's/^"//' -e 's/"$//' -e 's/%/$25/g' -e 's/\\n/%0A/g' -e 's/\\r/%0D/g' | \
+	sed -e 's/^"//' -e 's/"$//' -e 's/%/%25/g' -e s/\n/'%0A'/g -e s/\r/'%0D'/g | \
 	xargs -I {} echo "{}" >> $GITHUB_OUTPUT
