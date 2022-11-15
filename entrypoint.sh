f92d34148e0e623409536be2cf7f584281d72135
Igor Rodionov <goruha@gmail.com>
2022-11-15 21:55:35 +0300

    Added action inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,10 @@ set -e
 
 echo "${CONFIG}" | \
 	yq  -o json -M -e | \
-	jq -c -e -M "${QUERY} | to_entries | map(\"\(.key)=\(.value|tostring)\")|.[]" 
+	jq -c -e -M -r "${QUERY} | to_entries | map(\"\(.key)=\(.value|tostring)\")|.[]" | \
+	xargs -I {} echo "{}"
 
 echo "${CONFIG}" | \
 	yq  -o json -M -e | \
-	jq -c -e -M "${QUERY} | to_entries | map(\"\(.key)=\(.value|tostring)\")|.[]" | \
+	jq -c -e -M -r "${QUERY} | to_entries | map(\"\(.key)=\(.value|tostring)\")|.[]" | \
 	xargs -I {} echo "{}" >> $GITHUB_OUTPUT
