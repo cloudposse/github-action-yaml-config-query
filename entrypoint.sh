d06bfe900d4f4fd4e39ad597815487737c61937a
Igor Rodionov <goruha@gmail.com>
2022-11-15 21:50:49 +0300

    Added action inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 echo "${CONFIG}" | \
 	yq  -o json -M -e | \
 	jq -c -e -M "${QUERY} | to_entries | map(\"\(.key)=\(.value|tostring)\")|.[]" | \
-	xargs -I {} echo "{}"
+	xargs -I {} echo {}
 
 echo "${CONFIG}" | \
 	yq  -o json -M -e | \
