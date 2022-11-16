35bd0984b296c923dc0b0288bded3e663a7c1683
Igor Rodionov <goruha@gmail.com>
2022-11-16 03:10:18 +0300

    Initial implementation (#2)

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM alpine:3.11
+
+# Install the cloudposse alpine repository
+ADD https://apk.cloudposse.com/ops@cloudposse.com.rsa.pub /etc/apk/keys/
+RUN echo "@cloudposse https://apk.cloudposse.com/3.11/vendor" >> /etc/apk/repositories
+
+ENV YQ_VERSION=4.30.1-r0
+ENV JQ_VERSION=1.6-r0
+RUN apk add --no-cache bash \
+    jq@cloudposse==${JQ_VERSION} \
+    yq@cloudposse==${YQ_VERSION}
+
+ENTRYPOINT ["/bin/bash"]
+
+COPY entrypoint.sh /usr/local/bin/entrypoint
+
+CMD [ "-c", "entrypoint" ]
---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+set -e
+set -o pipefail
+
+OUTPUTS=(
+	$(
+		echo "${CONFIG}" | \
+		yq  -o json -M -e | \
+		jq -c -e -M  "${QUERY} | to_entries | map(\"\(.key)=\(.value|tostring)\")|.[]" | \
+		sed -e 's/^"//' -e 's/"$//'
+	)
+)
+
+## There is a bug with multiline output
+## Read this thread https://github.com/orgs/community/discussions/26288
+## Read this workaround https://trstringer.com/github-actions-multiline-strings/
+
+for item in "${OUTPUTS[@]}"
+do
+	NAME=$(echo ${item}  | sed -e 's/^\([^=]*\)=\(.*\)$/\1/g')
+	VALUE=$(echo ${item} | sed -e 's/^\([^=]*\)=\(.*\)$/\2/g')
+	echo "${NAME}<<EOF" >> $GITHUB_OUTPUT
+	echo -e "${VALUE//\\\"/\"}" >> $GITHUB_OUTPUT
+	echo "EOF" >> $GITHUB_OUTPUT
+done
