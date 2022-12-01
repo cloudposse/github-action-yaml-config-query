abb6d7f7ae94b881c385fa9ddc79d933d1d80f89
Igor Rodionov <goruha@gmail.com>
2022-12-01 17:04:50 +0300

    Added query test

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ OUTPUTS=(
 		echo "${CONFIG}" | \
 		yq -o json -M -e | \
 		jq . -c -e -M | \
-		jq -c -e -M -r  "${QUERY} | to_entries | map(\"\(.key)=\(.value|tostring)\")|.[]"
+		jq -c -e -M "${QUERY} | to_entries | map(\"\(.key)=\(.value|tostring)\")|.[]"
 	)
 )
 IFS="$oIFS"
